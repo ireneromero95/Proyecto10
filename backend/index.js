11ac30310c0f61ce735ff2564e5f8a55e1e98d7e
IreneRomero <irene_romero_95@hotmail.com>
2025-03-19 12:02:53 +0100

    error cors

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,13 @@ connectDB();
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(express.json());
 app.use('/api/v1/eventos', eventosRouter);
 app.use('/api/v1/users', usersRouter);
