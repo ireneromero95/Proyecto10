11485403f67e0b7aef9a5f0ea360e9a4d7fbd7da
IreneRomero <irene_romero_95@hotmail.com>
2025-04-23 10:24:03 +0200

    intentando cosas para el deploy

--- backend/index.js
+++ backend/index.js
@@ -18,12 +18,12 @@ connectDB();
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 app.use(express.json());
 app.use('/api/v1/eventos', eventosRouter);
